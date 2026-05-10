2347312b1112840cbb86708d1941bdef15dbcf4f
Dhritikrishna Tripathi <dhritikrishnat@gmail.com>
2026-05-10 18:17:13 +0530

    Add KINGDOM problem starter file

---
+++ kingdom.py
@@ -0,0 +1,37 @@
+"""
+KINGDOM - Identify which Indian empire ruled in a given year.
+
+India has been home to some of the most powerful empires in history.
+Given a year, identify which empire held power.
+
+Empires and their periods:
+    Maurya Empire:    322 BCE  to 185 BCE  (-322 to -185)
+    Gupta Empire:     320 CE  to  550 CE
+    Mughal Empire:   1526 CE  to 1857 CE
+    British Raj:     1858 CE  to 1947 CE
+
+If the year doesn't fall in any empire's range, print "No major empire".
+
+Note: Use negative numbers for BCE years. For example, 300 BCE = -300.
+
+Example:
+    Year: 300
+    Mughal Empire
+
+    Year: 400
+    Gupta Empire
+
+    Year: -200
+    Maurya Empire
+"""
+
+
+def main():
+    # TODO: Prompt the user for a year (negative for BCE)
+    # TODO: Determine which empire was in power
+    # TODO: Print the empire name
+    pass
+
+
+if __name__ == "__main__":
+    main()
